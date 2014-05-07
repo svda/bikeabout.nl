420d36c44903496c7b1d20d645f3942ccc19db68
Sander van den Akker <sander@myxt.nl>
2014-05-08 00:23:54 +0200

    Added 404. Hints scaffolding.

--- client/main.js
+++ client/main.js
@@ -1,51 +1,38 @@
-// Meteor settings
-
-Accounts.config({
-  forbidClientAccountCreation: true
-});
-
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
-});
-
-// Package settings
-
-c2o.Tracker.register(GoogleAnalyticsTracker, {account: 'UA-5140591-3'})
-
-SEO.configure({
-  baseTitle: ' - BikeAbout'
-});
-
 var commentsLoaded = false;
 Template.tumblrFeedPost.rendered = function () {
   commentsLoaded = false;
+  update();
+
+  function update () {
+    commentsLoaded = true;
+    var thread = $('<div id="disqus_thread">').appendTo('article.post');
+    var disqus_shortname = Meteor.settings.public.disqus.shortname; // required: replace example with your forum shortname
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  }
+
   $(document).on('scroll', function (e) {
-    if(isScrolledIntoView('.comments') && !commentsLoaded) {
-      commentsLoaded = true;
-      var thread = $('<div id="disqus_thread">').appendTo('article.post');
-      var disqus_shortname = Meteor.settings.public.disqus.shortname; // required: replace example with your forum shortname
-      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    if(UI.isScrolledIntoView('.comments') && !commentsLoaded) {
+      update();
     }
   });
-}
+};
+
+Template.layout.events({
+  'click #washi': function (e) {
+    $('body').removeClass('menu-open');
+  }
+});
 
 Template.menu.rendered = function () {
   Meteor.defer(function() {
     $('.menu.animated').addClass('animate-in');
   });
-}
-
-isScrolledIntoView = function (elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
+  $('#primary-menu-btn').on('click', function (e) {
+    $('body').addClass('hide-hints');
+  })
+};
 
 $('a[role=navigation]').on( 'click', function (e) {
   $('html,body').animate({scrollTop: $('body').offset().top});
